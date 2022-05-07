83c04bbe6d109c890b7a59cf0efe3b58b55ab398
o0alvin0o <o0cesc0o@gmail.com>
2022-05-07 16:42:50 +0700

    act8_2

---
+++ src/lesson8/act8_2/LinkedListTest.java
@@ -0,0 +1,19 @@
+package lesson8.act8_2;
+//About 315 miliseconds
+import java.util.*;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        final int TEST_LIMIT = 10000, MAX_VALUE = 100000, TEST_INDEX = 50000;
+        List<Integer> arList = new LinkedList<>();
+        for (int i = 0; i < MAX_VALUE; i++) { // initial arrray
+            arList.add(i);
+        }
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < TEST_LIMIT / 2; i++) {
+            arList.remove((int)TEST_INDEX);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.print("Execute time: " + (endTime - startTime) + "miliseconds" );
+    }
+}
